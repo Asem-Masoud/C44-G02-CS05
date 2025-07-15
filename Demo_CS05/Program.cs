9927b1079308f0385ecf1a6d3a6aeca0c4f17075
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-07-15 15:13:17 +0300

    Solve Errors

--- Demo_CS05/Program.cs
+++ Demo_CS05/Program.cs
@@ -2,8 +2,87 @@
 {
     public class Program
     {
+        /*static void DoSomeCode()
+        {
+            int x, y, z;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            z = x / y;
+
+            int[] Arr = { 1, 2, 3 };
+
+            Arr[99] = 10;
+        }*/
+
+        /* static void DoSomeCode()
+         {
+             try
+             {
+                 int x, y, z;
+
+                 x = int.Parse(Console.ReadLine());
+                 y = int.Parse(Console.ReadLine());
+
+                 z = x / y;
+
+                 int[] Arr = { 1, 2, 3 };
+
+                 Arr[99] = 10;
+             }
+             catch (Exception e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+             finally
+             {
+                 Console.WriteLine("Finally");
+                 // Release
+                 // Deallocate
+                 // Disconnect
+                 // Dispose UnManged Resource.
+             }
+         }*/
+
+        /* static void DoSomeProtectiveCode()
+         {
+             try
+             {
+                 int x, y, z;
+
+                 bool flag;
+                 do
+                 {
+                     Console.Write("Enter 1st Number: ");
+                     flag = int.TryParse(Console.ReadLine(), out x);
+                 } while (flag == false);
+
+                 do
+                 {
+                     Console.Write("Enter 2nd Number: ");
+                     flag = int.TryParse(Console.ReadLine(), out y);
+                 } while (flag == false || y == 0);
+
+                 z = x / y;
+
+                 int[] Arr = { 1, 2, 3 };
+
+                 if (99 < Arr?.Length)
+                 {
+                     Arr[99] = 10;
+                 }
+                 Console.WriteLine(Arr[99]);
+             }
+             catch (Exception e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+
+         }*/
+
         static void Main()
         {
+
 
             // Nullable Types
             //Value Type
@@ -33,7 +112,7 @@
             //// Y : Allow int or null
 
 
-            //int? Y = /*(int)*/ X;
+            //int? Y = /*(int)*/
 
             //// Implicit Casting
             //// Safe Casting
@@ -121,7 +200,7 @@
             //Console.WriteLine(X);
             //Console.WriteLine(c);
 
-            int[] Arr = default; //null
+            //int[] Arr = default; //null
 
             //int[] Arr = { 1, 2, 3, 4, 5 };
 
@@ -147,24 +226,40 @@
 
             //int? len = Arr?.Length;
 
-            int len = Arr?.Length ?? -1;
+            //int len = Arr?.Length ?? -1;
 
 
-            Console.WriteLine(len);
-
-
-
-
-
-
+            //Console.WriteLine(len);
 
             #endregion
 
             #region 05. Exception Handling And Protective Code
 
+            //DoSomeCode();
 
+
+
+            //ApplicationException;
+            // Exception
+            // 1. SystemException
+            //   1.1 FormatException
+            //   1.2 IndexOfException
+            //   1.3 NullReferenceException
+            //   1.4 ArithemticException
+            //      1.4.1 OverFlowException
+            //      1.4.1 DevideException
+            // 2. ApplicationException
+
+            //DoSomeProtectiveCode();
+
+
+
+            Console.WriteLine("Hello After Exception");
 
             #endregion
+
+
+
 
         }
     }
