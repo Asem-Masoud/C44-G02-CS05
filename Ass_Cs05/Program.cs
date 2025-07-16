23be4260a8698883bbe1a3d18388aa78f7c92b19
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-07-16 03:57:59 +0300

    Q2P02

--- Ass_Cs05/Program.cs
+++ Ass_Cs05/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Ass_Cs05
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+namespace Ass_Cs05
 {
     enum WeekDays
     {
@@ -10,6 +13,15 @@
         Saturday,
         Sunday
     }
+
+    enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+
     public class Program
     {
         static void Main()
@@ -18,11 +30,46 @@
 
             #region Q1
             //1. Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
-            Console.WriteLine("Days of the week:");
+            //Console.WriteLine("Days of the week:");
 
-            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+
+            #endregion
+
+            #region Q2
+
+            // Create an enum called "Season" with the four seasons(Spring, Summer, Autumn, Winter) as its members.Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season.
+            // Note range for seasons(spring march to may, summer june to august, autumn September to November, winter December to February)
+
+            Console.Write("Enter a season name: ");
+            string input = Console.ReadLine();
+
+            if (Enum.TryParse(typeof(Season), input, true, out object result))
             {
-                Console.WriteLine(day);
+                Season Select = (Season)result;
+
+                switch (Select)
+                {
+                    case Season.Spring:
+                        Console.WriteLine(Season.Spring + ": March to May");
+                        break;
+                    case Season.Summer:
+                        Console.WriteLine(Season.Summer + ": June to August");
+                        break;
+                    case Season.Autumn:
+                        Console.WriteLine(Season.Autumn + ": September to November");
+                        break;
+                    case Season.Winter:
+                        Console.WriteLine(Season.Winter + ": December to February");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid season name! Please enter one of: Spring, Summer, Autumn, Winter.");
             }
 
             #endregion
